64a314303d7db1d0d861771a841fcc92599f1f88
Dhruv Patel <pateld2253@gmail.com>
2025-06-27 12:51:19 -0400

    Imported mongoose and wrote all the routes for the habits

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,18 @@ import express from "express"; //if you're using import just make sure that type
 
 const app = express(); //this is to make it run
 
+app.get("/api/habits", (req, res) => {
+  res.status(200).send("You got 10 habits"); //this is what will be sent and displayed
+});
+app.post("/api/habits", (req, res) => {
+  res.status(201).json({ message: "Habit created" }); //this will send when its succesfully created
+});
+app.put("/api/habit:id", (req, res) => {
+  res.status(201).json({ message: "Habit updated successfully" }); //this will send when its succesfully updated
+});
+app.delete("/api/habit:id", (req, res) => {
+  res.status(201).json({ message: "Habit deleted successfully" }); //this will send when its succesfully deleted
+});
 app.listen(5001, () => {
   console.log("Server started on PORT: 5001");
 });
