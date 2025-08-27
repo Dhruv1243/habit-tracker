eca2035f967c594eee5271f2d201e5c94094bf2e
Dhruv Patel <pateld2253@gmail.com>
2025-08-26 21:22:53 -0400

    Home page done with css

--- frontend/habit-tracker/src/main.jsx
+++ frontend/habit-tracker/src/main.jsx
@@ -1,10 +1,14 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
 
-createRoot(document.getElementById('root')).render(
+import App from "./App.jsx";
+
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    {/*Thanks to this we can now use the routing in our entire app */}
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </StrictMode>
+);
---
+++ frontend/habit-tracker/src/pages/HomePage.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+import "../css/HomePage.css";
+
+const HomePage = () => {
+  return (
+    <div>
+      <div className="main-container">
+        <div className="element-container">
+          <div className="text-container">
+            <h1>Ready To Reach Your Full Potential?</h1>
+            <p>Your journey to growth and consistency starts here.</p>
+          </div>
+          <div className="button-container">
+            <button>Get Started</button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default HomePage;
